933bdf32638dca28455e9c454d999f9842888675
Akshansh <thebigolwiseone@gmail.com>
2025-03-25 19:36:11 +0530

    cleaner stuff

--- build.bat
+++ build.bat
@@ -1,2 +1,13 @@
 @echo off
-octave --persist compression.m cat_sample.jpg gray
+echo Choose an option:
+echo [1] Run Command A
+echo [2] Run Command B
+set /p choice="Enter your choice [1/2]: "
+
+if "%choice%"=="1" (
+    octave --persist compression.m cat_sample.jpg rgb
+) else if "%choice%"=="2" (
+    octave --persist compression.m cat_sample.jpg gray
+) else (
+    echo Invalid choice. Exiting.
+)
